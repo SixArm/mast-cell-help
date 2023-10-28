e168362ec1e5791109be7896d615dde07064ab41
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-10-27 17:01:08 -0700

    Add support-groups/index.m

---
+++ topics/support-groups/index.m
@@ -0,0 +1,21 @@
+# Support groups
+
+A serious medical condition can have significant impact on a person's health and quality of life. Support groups can be valuable resources for individuals, as they provide a platform for sharing experiences, seeking advice, and finding emotional support from others who understand the challenges associated with the condition.
+
+Here are some places where you can find support groups and communities…
+
+* Online Forums and Social Media Groups: Many support groups exist on popular social media platforms and health-related forums. Websites like Reddit, Facebook, and patient support forums host groups where members can share their experiences, ask questions, and offer advice.
+
+* Local Meetup Groups: Check websites like Meetup.com for local support groups or rare disease support groups. These groups often organize in-person meetings, which can be an excellent way to connect with other people in your area.
+
+* Patient Advocacy Organizations: Various patient advocacy and rare disease organizations provide resources and support for individuals. Examples include The Mastocytosis Society and the Mast Cell Action Group. These organizations often have online communities and resources for patients.
+
+* Healthcare Provider Recommendations: Your healthcare provider may be aware of local or online support groups. They can provide guidance on finding appropriate resources.
+
+* Online Health Communities: Websites and platforms like Inspire, HealthUnlocked, and RareConnect host various health communities where people can connect, share information, and find support.
+
+* Social Media Hashtags: You can use relevant hashtags on social media platforms like X, Threads, and Instagram to connect with others. For example, you can search for or use hashtags like #MCAS, #MastCellActivationSyndrome, or #RareDisease to discover related posts and accounts.
+
+When participating in support groups, it's important to remember that while these communities can be a source of valuable information and emotional support, they should not replace medical advice from qualified healthcare providers. Always consult with your healthcare team for personalized guidance and treatment recommendations.
+
+Additionally, consider privacy and security when sharing personal information in online support groups, and be cautious about disclosing sensitive medical details. Support groups can be a wonderful source of camaraderie and knowledge, but it's essential to exercise appropriate caution and respect within these communities.

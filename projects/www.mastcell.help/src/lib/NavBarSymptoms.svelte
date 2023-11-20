f1f6fb33218c447c85db1a262ab76a177369afd4
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-11-20 13:03:36 -0800

    Add NavBarSymptoms.svelte

---
+++ projects/www.mastcell.help/src/lib/NavBarSymptoms.svelte
@@ -0,0 +1,9 @@
+<ul class="app-navbar-menu-list">
+    <li><a href="/bradycardia">Bradycardia</a></li>
+    <li><a href="/brain-fog">Brain fog</a></li>
+    <li><a href="/bronchospasm">Bronchospasm</a></li>
+    <li><a href="/hypertryptasemia">Hypertryptasemia</a></li>
+    <li><a href="/hypotension">Hypotension</a></li>
+    <li><a href="/presyncope">Presyncope</a></li></ul>
+</ul>
+

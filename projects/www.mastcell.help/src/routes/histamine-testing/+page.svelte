9ee4768c10d5e77b67e469cf3bdc8f0b61b8f948
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-11-27 14:11:30 -0800

    Add topics

---
+++ projects/www.mastcell.help/src/routes/histamine-testing/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+    <title>Histamine testing | My Example Site | mastcell.help</title>
+</svelte:head>
+
+<div class="prose">
+
+<h1 id="histamine-testing">Histamine testing</h1>
+<p>Histamine testing is often done to assess the levels of histamine in the body, particularly in cases where there is suspicion of histamine intolerance or other conditions related to histamine regulation. Histamine is a chemical involved in several bodily functions, including the immune response, regulation of stomach acid, and neurotransmission.</p>
+<p>Common methods used for histamine testing:</p>
+<ul>
+<li><p>Blood Plasma Histamine Test: This test measures the concentration of histamine in the blood plasma. Blood is drawn, and the sample is analyzed to determine if histamine levels are within the normal range. Elevated levels may indicate histamine intolerance or other conditions.</p></li>
+<li><p>Blood Diamine Oxidase (DAO) Activity Test: DAO is an enzyme that breaks down histamine in the body. A blood test can measure the activity of DAO, and low DAO activity may suggest an inability to effectively break down histamine, potentially contributing to histamine intolerance.</p></li>
+<li><p>Urine Tests: Some tests measure histamine and its metabolites in urine. Elevated levels in urine may suggest an excess of histamine in the body.</p></li>
+<li><p>Provocation Test: In this test, a patient is given a controlled dose of histamine, usually through injection or ingestion, and their response is observed. This can help assess how the body reacts to histamine exposure.</p></li>
+</ul>
+<p>It’s important to note that while these tests may provide information about histamine levels, diagnosing histamine intolerance or other related conditions can be challenging. Elevated histamine levels alone may not necessarily indicate a specific disorder, as histamine is involved in various physiological processes.</p>
+
+</div>
